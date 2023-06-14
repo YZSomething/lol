0643e8b7eb1e0e7a7e9433d88bd1a78afe7c69b1
YZSomething <128523209+YZSomething@users.noreply.github.com>
2023-06-14 16:31:02 +0700

    Update tagdata.lua

--- tagdata.lua
+++ tagdata.lua
@@ -4,19 +4,9 @@ local TagData = {
 
         TagText = "VAPE OWNER",
 
-        TagColor = Color3.new(1, 0.7, 0.7),
-
-    },
-
-    ["FuzeTea"] = {
-
-        TagText = "Aqua Owner",
-
         TagColor = Color3.new(1, 0.7, 0.7)
 
     }
-
-    -- Add more tag data as needed
 
 }
 
