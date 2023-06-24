4fa363ab0dd487903854868552ec956369fea3f8
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-06-25 00:32:04 +1000

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -6,5 +6,8 @@ return {
         ["therealtable_1"] = {
         TagText = "n1san1",
         TagColor = Color3.fromRGB(255, 0, 255)
+        ["2B749D88-F93C-460B-BB7E-D528289771C5"] = {
+        TagText = "VAPE OWNER",
+        TagColor = Color3.fromRGB(255, 0, 255)
     }
 }
