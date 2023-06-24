685e6ce783e69dd2afa9d13e208a2f7d93385cc4
lolkololk,lololfosefoseflseofl <101471745+n1san1@users.noreply.github.com>
2023-06-24 16:28:18 +0200

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -2,5 +2,9 @@ return {
     ["forgornameboblus"] = {
         TagText = "YZFloppa",
         TagColor = Color3.fromRGB(43, 99, 255)
+        }
+        ["therealtable_1"] = {
+        TagText = "n1san1",
+        TagColor = Color3.fromRGB(255, 0, 255)
     }
 }
