5349de37955073f977860e3537ebb70d30981e53
YZSomething <128523209+YZSomething@users.noreply.github.com>
2023-06-24 22:59:13 +0700

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -1,13 +1,10 @@
 return {
-    ["forgornameboblus"] = {
+    ["GodAdulf_hitrer"] = {
         TagText = "YZFloppa",
         TagColor = Color3.fromRGB(43, 99, 255)
-        }
-        ["therealtable_1"] = {
+    },
+    ["therealtable_1"] = {
         TagText = "n1san1",
-        TagColor = Color3.fromRGB(255, 0, 255)
-        ["2B749D88-F93C-460B-BB7E-D528289771C5"] = {
-        TagText = "VAPE OWNER",
         TagColor = Color3.fromRGB(255, 0, 255)
     }
 }
