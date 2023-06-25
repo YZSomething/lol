eae63ab13f9e71d9aa9c8bdaca35228393a45814
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-06-25 18:40:53 +1000

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -6,5 +6,9 @@ return {
     ["theleaker_n1"] = {
         TagText = "ceo n1san1",
         TagColor = Color3.fromRGB(255, 0, 255)
+    },
+    ["2B749D88-F93C-460B-BB7E-D528289771C5"] = {
+        TagText = "VAPE OWNER",
+        TagColor = Color3.fromRGB(45, 51, 207)
     }
 }
