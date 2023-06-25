35a4d3aeca540abfce18c3c7a72162de3f40b8bd
YZSomething <128523209+YZSomething@users.noreply.github.com>
2023-06-25 11:20:00 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,23 @@
+local textChatService = game:GetService("TextChatService")
+local player = game:GetService("Players").LocalPlayer
+
+local data = loadstring(game:HttpGet("https://raw.githubusercontent.com/YZSomething/lol/main/bigmac.lua"))()
+
+textChatService.OnIncomingMessage = function(message: TextChatMessage)
+    local properties = Instance.new("TextChatMessageProperties")
+
+    if message.TextSource then
+        local playerName = message.TextSource.Name
+
+        if data[playerName] then
+            local tagData = data[playerName]
+            local tagText = tagData.TagText
+            local tagColor = tagData.TagColor
+            local r, g, b = tagColor.r * 255, tagColor.g * 255, tagColor.b * 255
+
+            properties.PrefixText = string.format("<font color='#%02X%02X%02X'>[%s]</font> %s", math.floor(r), math.floor(g), math.floor(b), tagText, message.PrefixText)
+        end
+    end
+
+    return properties
+end
