b72c409cd0274044678b0b6f458c43fddd3a56f6
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-06-25 18:43:36 +1000

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -7,7 +7,7 @@ return {
         TagText = "ceo n1san1",
         TagColor = Color3.fromRGB(255, 0, 255)
     },
-    ["2B749D88-F93C-460B-BB7E-D528289771C5"] = {
+    ["SubToStavXpl0itz"] = {
         TagText = "VAPE OWNER",
         TagColor = Color3.fromRGB(45, 51, 207)
     }
