d5abdf16db129d074a8a36ab11bb44da6b4c969d
lolkololk,lololfosefoseflseofl <101471745+n1san1@users.noreply.github.com>
2023-06-25 15:30:53 +0200

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -3,8 +3,8 @@ return {
         TagText = "YZFloppa",
         TagColor = Color3.fromRGB(43, 99, 255)
     },
-    ["theleaker_n1"] = {
-        TagText = "ceo n1san1",
+    ["ikilledpeoplein9_i11"] = {
+        TagText = "n1san1 made 9/11",
         TagColor = Color3.fromRGB(255, 0, 255)
     },
     ["SubToStavXpl0itz"] = {
