97069339b1025ed1e625573113a2982e6da82cd1
YZSomething <128523209+YZSomething@users.noreply.github.com>
2023-06-26 12:31:25 +0700

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -2,9 +2,5 @@ return {
     ["ilikeadulf_hitrer"] = {
         TagText = "Adolf Hitler",
         TagColor = Color3.fromRGB(43, 99, 255)
-    },
-    ["ikilledpeoplein9_i11"] = {
-        TagText = "n1san1 made 9/11",
-        TagColor = Color3.fromRGB(255, 0, 255)
     }
 }
