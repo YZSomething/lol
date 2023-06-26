75b3351fce4fb4d6a14178207502b7cac42a2400
YZSomething <128523209+YZSomething@users.noreply.github.com>
2023-06-26 12:47:49 +0700

    Update bigmac.lua

--- bigmac.lua
+++ bigmac.lua
@@ -2,9 +2,7 @@ return {
     ["ilikeadulf_hitrer"] = {
         TagText = "ADOLF HITLER",
         TagColor = Color3.fromRGB(43, 99, 255)
-    }
-}
-return {
+    },
     ["ChanceChang3d"] = {
         TagText = "VAPE OWNER"
         TagColor = Color3.fromRGB(125, 0, 125)
